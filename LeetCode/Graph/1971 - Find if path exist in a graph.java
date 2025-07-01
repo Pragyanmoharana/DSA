08d54f6e1da0ebc91f35d8be7727d97f51386fef
Pragyan Bharati Moharana <165978755+Pragyanmoharana@users.noreply.github.com>
2025-07-02 00:47:41 +0530

    Update 1971 - Find if path exist in a graph.java

--- LeetCode/Graph/1971 - Find if path exist in a graph.java
+++ LeetCode/Graph/1971 - Find if path exist in a graph.java
@@ -1,1 +1,38 @@
+class Solution {
 
+
+
+public boolean dfs(List<List<Integer>> graph, int src, int dest, boolean[] vis){
+    if (src == dest) return true;
+    vis[src] = true;
+
+    for (int i = 0; i < graph.get(src).size(); i++) {
+        int neighbor = graph.get(src).get(i);
+        if (!vis[neighbor] && dfs(graph, neighbor, dest, vis)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
+
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        //step1-create the graph
+         List<List<Integer>> graph=new ArrayList<>();
+         for(int i=0;i<n;i++){
+            graph.add(new ArrayList<>());
+         }
+         //step 2 add edges of(unidirected)
+         for(int[] edge:edges){
+            int u=edge[0];
+            int v=edge[1];
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+         }
+         //step 3:Run DFS
+          boolean[] vis=new boolean[n];
+          return dfs(graph,source,destination,vis);
+    }
+}
